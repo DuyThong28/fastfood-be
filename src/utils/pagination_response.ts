81b96eeb6143a3a844261b9cc83f4e4ade33e0fc
Lmt310104 <lmtoan311@gmail.com>
2024-10-29 09:05:26 +0700

    feat: set up object response and pagination

--- src/utils/pagination_response.ts
+++ src/utils/pagination_response.ts
@@ -1,8 +1,31 @@
-import { IsEnum, IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsEnum, IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
+import { MAX_ITEMS_PER_PAGE } from 'src/constants/constraints';
 import { ORDER } from 'src/constants/enum';
 
 export class PaginationResponse {
   @IsEnum(ORDER)
   @IsOptional()
   order?: ORDER = ORDER.DESC;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  page?: number = 1;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(MAX_ITEMS_PER_PAGE)
+  @IsOptional()
+  take?: number;
+
+  get skip(): number {
+    return (this.page - 1) * this.take;
+  }
+
+  @IsString()
+  @IsOptional()
+  sortBy?: string = 'createdAt';
 }
