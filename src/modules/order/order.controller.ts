b1182526023b34c5bc40dca618d7ecf28d62dbe8
Lmt310104 <lmtoan311@gmail.com>
2024-11-07 07:47:18 +0700

    feat: order

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,7 +1,133 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import {
+  TUserSession,
+  UserSession,
+} from 'src/common/decorators/user-session.decorator';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Orders, Reviews } from '@prisma/client';
+import { StandardResponse } from 'src/utils/response.dto';
+import { END_POINTS } from 'src/constants/end_points';
+import { ROLE } from 'src/constants/enum';
 import { OrderService } from './order.service';
+import { OrderPageOptionsDto } from './dto/find_all_order.dto';
+import { PageResponseMetaDto } from 'src/utils/page_response_meta.dto';
+import { PageResponseDto } from 'src/utils/page_response.dto';
+import HttpStatusCode from 'src/constants/http_status_code';
+import { CreateOrderDto } from './dto/create_order.dto';
+import { CreateReviewDto } from './dto/create_review.dto';
+import { UpdateOrderStatusDto } from './dto/update_order_status.dto';
 
-@Controller('order')
-export class OrderController {
+const {
+  ORDER: {
+    BASE,
+    GET_FULL_LIST,
+    GET_ALL,
+    CREATE,
+    UPDATE_STATUS,
+    GET_ONE,
+    CANCEL_ORDER,
+    GET_ONE_BY_ADMIN,
+  },
+} = END_POINTS;
+
+@Controller(BASE)
+export class OrdersController {
   constructor(private readonly orderService: OrderService) {}
+  @Get(GET_FULL_LIST)
+  @Roles(ROLE.ADMIN)
+  async getListOrders(
+    @Query() query: OrderPageOptionsDto,
+  ): Promise<PageResponseDto<Orders>> {
+    const { orders, itemCount } = await this.orderService.getListOrders(query);
+    const meta = new PageResponseMetaDto({
+      pageOptionsDto: query,
+      itemCount: itemCount,
+    });
+    return new PageResponseDto(orders, meta);
+  }
+  @Get(GET_ALL)
+  async getAllOrders(
+    @Query() query: OrderPageOptionsDto,
+    @UserSession() session: TUserSession,
+  ): Promise<PageResponseDto<Orders>> {
+    const { orders, itemCount } = await this.orderService.getListOrdersByUser(
+      query,
+      session,
+    );
+    const meta = new PageResponseMetaDto({
+      pageOptionsDto: query,
+      itemCount: itemCount,
+    });
+    return new PageResponseDto(orders, meta);
+  }
+  @Post(CREATE)
+  async createOrder(
+    @UserSession() session: TUserSession,
+    @Body() dto: CreateOrderDto,
+  ) {
+    const order = await this.orderService.createOrder(session, dto);
+    const message = 'Order created successfully';
+    return new StandardResponse<Orders>(order, message, HttpStatusCode.CREATED);
+  }
+  @Post(UPDATE_STATUS)
+  async updateOrderStatus(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() dto: UpdateOrderStatusDto,
+  ) {
+    const order = await this.orderService.updateOrderStatus(id, dto);
+    const message = 'Order status updated successfully';
+    return new StandardResponse<Orders>(order, message, HttpStatusCode.OK);
+  }
+  @Get(GET_ONE_BY_ADMIN)
+  async getOrderDetailsByAdmin(@Param('id', ParseUUIDPipe) id: string) {
+    const order = await this.orderService.getOrderDetailsByAdmin(id);
+    const message = 'Order details retrive';
+    return new StandardResponse<Orders>(order, message, HttpStatusCode.OK);
+  }
+  @Get(GET_ONE)
+  async getOrderDetails(
+    @UserSession() session: TUserSession,
+    @Param('id', ParseUUIDPipe) id: string,
+  ): Promise<StandardResponse<Orders>> {
+    const order = await this.orderService.getOrderProductsByUser(id, session);
+    const message = 'Order details retrieved successfully';
+    return new StandardResponse<Orders>(order, message, HttpStatusCode.OK);
+  }
+  @Post(`${GET_ONE}/order-details/:orderDetailsId/:bookId`)
+  async createReview(
+    @UserSession() session: TUserSession,
+    @Body() dto: CreateReviewDto,
+    @Param('id', ParseUUIDPipe) id: string,
+    @Param('orderDetailsId', ParseUUIDPipe) orderDetailsId: string,
+    @Param('bookId', ParseUUIDPipe) bookId: string,
+  ): Promise<StandardResponse<Reviews>> {
+    console.log(dto);
+    const review = await this.orderService.createReview(
+      session,
+      dto,
+      id,
+      orderDetailsId,
+      bookId,
+    );
+    const message = 'Comment created successfully';
+    return new StandardResponse(review, message, HttpStatusCode.CREATED);
+  }
+  @Patch(CANCEL_ORDER)
+  async cancelOrder(
+    @Param('id', ParseUUIDPipe) id: string,
+    @UserSession() session: TUserSession,
+  ) {
+    const order = await this.orderService.cancelOrder(id, session);
+    const message = 'Order cancelled successfully';
+    return new StandardResponse(order, message, HttpStatusCode.OK);
+  }
 }
--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,4 +1,351 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { TUserSession } from 'src/common/decorators/user-session.decorator';
+import { OrderStatus } from '@prisma/client';
+import { OrderPageOptionsDto } from './dto/find_all_order.dto';
+import { CreateOrderDto } from './dto/create_order.dto';
+import { CreateReviewDto } from './dto/create_review.dto';
+import { ORDER_STATUS } from 'src/constants/enum';
+import { UpdateOrderStatusDto } from './dto/update_order_status.dto';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+  constructor(private readonly prisma: PrismaService) {}
+  async createOrder(session: TUserSession, dto: CreateOrderDto) {
+    const bookIds = dto.items.map((item) => item.bookId);
+    const books = await this.prisma.books.findMany({
+      where: { id: { in: bookIds } },
+    });
+    const cart = await this.prisma.carts.findFirstOrThrow({
+      where: { user_id: session.id },
+    });
+    const cartItems = await this.prisma.cartItems.findMany({
+      where: { cart_id: cart.id, book_id: { in: bookIds } },
+    });
+    const cartItemIds = cartItems.map((item) => item.id);
+    if (books.length !== bookIds.length) {
+      throw new NotFoundException('Some books are not found');
+    }
+    const bookPriceMap = new Map(
+      books.map((book) => [
+        book.id,
+        { price: book.price, finalPrice: book.final_price ?? book.price },
+      ]),
+    );
+    try {
+      return await this.prisma.$transaction(async (tx) => {
+        await tx.cartItems.deleteMany({
+          where: {
+            id: { in: cartItemIds },
+          },
+        });
+        const order = await tx.orders.create({
+          data: {
+            user_id: session.id,
+            full_name: dto.fullName,
+            phone_number: dto.phoneNumber,
+            address: dto.address,
+          },
+        });
+        const orderItems = dto.items.map((item) => {
+          const { price, finalPrice } = bookPriceMap.get(item.bookId);
+          const totalPrice = Number(finalPrice) * item.quantity;
+          return {
+            order_id: order.id,
+            book_id: item.bookId,
+            quantity: item.quantity,
+            price,
+            total_price: totalPrice,
+          };
+        });
+        await tx.orderItems.createMany({ data: orderItems });
+        await Promise.all(
+          orderItems.map((item) =>
+            tx.books.update({
+              where: { id: item.book_id },
+              data: {
+                stock_quantity: { decrement: item.quantity },
+                sold_quantity: { increment: item.quantity },
+              },
+            }),
+          ),
+        );
+        const totalPrice = orderItems.reduce(
+          (acc, item) => acc + item.total_price,
+          0,
+        );
+        const updatedOrder = await tx.orders.update({
+          where: { id: order.id },
+          data: {
+            total_price: totalPrice,
+          },
+          include: {
+            OrderItems: {
+              include: {
+                book: true,
+              },
+            },
+          },
+        });
+        return updatedOrder;
+      });
+    } catch (error) {
+      console.log('Error:', error);
+      throw new Error('Failed to create order');
+    }
+  }
+  async getListOrders(query: OrderPageOptionsDto) {
+    const { take, order, sortBy } = query;
+    const orders = await this.prisma.orders.findMany({
+      where: {
+        ...(query.status && { status: query.status }),
+        ...(query.search && {
+          id: { contains: query.search, mode: 'insensitive' },
+        }),
+      },
+      skip: query.skip,
+      take: take,
+      orderBy: { [sortBy]: order },
+      include: {
+        OrderItems: {
+          include: {
+            book: true,
+          },
+        },
+        user: {
+          select: {
+            id: true,
+            email: true,
+            full_name: true,
+          },
+        },
+      },
+    });
+    const itemCount = await this.prisma.orders.count({
+      where: {
+        ...(query.status && { status: query.status }),
+        ...(query.search && {
+          id: { contains: query.search, mode: 'insensitive' },
+        }),
+      },
+    });
+    return { orders, itemCount };
+  }
+  async getOrderProductsByUser(id: string, session: TUserSession) {
+    const order = await this.prisma.orders.findUnique({
+      where: { user_id: session.id, id: id },
+      include: {
+        OrderItems: {
+          include: {
+            book: true,
+          },
+        },
+      },
+    });
+    return order;
+  }
+  async getListOrdersByUser(query: OrderPageOptionsDto, session: TUserSession) {
+    const { take, order, sortBy } = query;
+    const orders = await this.prisma.orders.findMany({
+      where: {
+        user_id: session.id,
+        ...(query.status && { status: query.status }),
+        ...(query.search && {
+          id: { contains: query.search, mode: 'insensitive' },
+        }),
+      },
+      skip: query.skip,
+      take: take,
+      orderBy: { [sortBy]: order },
+      include: {
+        OrderItems: {
+          include: {
+            book: true,
+          },
+        },
+      },
+    });
+    const itemCount = await this.prisma.orders.count({
+      where: {
+        user_id: session.id,
+        ...(query.status && { status: query.status }),
+        ...(query.search && {
+          id: { contains: query.search, mode: 'insensitive' },
+        }),
+      },
+    });
+    return { orders, itemCount };
+  }
+  async updateOrderStatus(id: string, dto: UpdateOrderStatusDto) {
+    const order = await this.prisma.orders.findUnique({
+      where: { id },
+    });
+    if (!order) {
+      throw new NotFoundException('Order not found');
+    }
+    if (
+      order.status === ORDER_STATUS.CANCELLED ||
+      order.status === ORDER_STATUS.REJECT
+    ) {
+      throw new BadRequestException('Order already cancelled or rejected');
+    }
+    if (dto.status === ORDER_STATUS.REJECT) {
+      try {
+        return await this.prisma.$transaction(async (tx) => {
+          const updatedOrder = await tx.orders.update({
+            where: { id },
+            data: { status: dto.status },
+          });
+          return updatedOrder;
+        });
+      } catch (error) {
+        console.log(error);
+        throw new BadRequestException('Failed to update order status');
+      }
+    }
+    return await this.prisma.orders.update({
+      where: { id },
+      data: { status: dto.status },
+    });
+  }
+  async createReview(
+    session: TUserSession,
+    dto: CreateReviewDto,
+    id: string,
+    orderDetailId: string,
+    bookId: string,
+  ) {
+    const order = await this.prisma.orders.findUnique({
+      where: { user_id: session.id, id },
+    });
+    if (!order) {
+      throw new NotFoundException('Order not found');
+    }
+    const orderDetail = await this.prisma.orderItems.findUnique({
+      where: { id: orderDetailId },
+    });
+    if (!orderDetail) {
+      throw new NotFoundException('Order detail not found');
+    }
+    const book = await this.prisma.books.findUnique({
+      where: { id: bookId },
+    });
+    if (!book) {
+      throw new NotFoundException('Book not found');
+    }
+    try {
+      return await this.prisma.$transaction(async (tx) => {
+        const newTotalReviews = book.total_reviews + 1;
+        const newAvgStars =
+          (Number(book.avg_stars) * book.total_reviews + dto.star) /
+          newTotalReviews;
+        await tx.books.update({
+          where: { id: book.id },
+          data: {
+            total_reviews: newTotalReviews,
+            avg_stars: newAvgStars,
+          },
+        });
+        const review = await tx.reviews.create({
+          data: {
+            user_id: session.id,
+            book_id: book.id,
+            rating: dto.star,
+            description: dto.description,
+            title: dto.title,
+          },
+          include: {
+            book: true,
+          },
+        });
+        return review;
+      });
+    } catch (error) {
+      console.log('Error:', error);
+      throw new BadRequestException({
+        message: 'Failed to add rating review',
+      });
+    }
+  }
+  async cancelOrder(id: string, session: TUserSession) {
+    const order = await this.prisma.orders.findUnique({
+      where: { id: id, user_id: session.id },
+    });
+    if (!order) {
+      throw new NotFoundException('Order not found');
+    }
+    if (order.status === ORDER_STATUS.CANCELLED) {
+      throw new BadRequestException('Order already cancelled');
+    }
+    const orderDetails = await this.prisma.orderItems.findMany({
+      where: { order_id: id },
+    });
+    const bookIds = orderDetails.map((item) => {
+      return { id: item.book_id, quantity: item.quantity };
+    });
+    try {
+      return await this.prisma.$transaction(async (tx) => {
+        await tx.orders.update({
+          where: { id },
+          data: { status: ORDER_STATUS.CANCELLED as OrderStatus },
+        });
+        bookIds.forEach(async (item) => {
+          await tx.books.update({
+            where: { id: item.id },
+            data: {
+              stock_quantity: { increment: item.quantity },
+            },
+          });
+        });
+        return await tx.orders.findUnique({
+          where: { id },
+        });
+      });
+    } catch (error) {
+      console.log('Error:', error);
+      throw new BadRequestException('Failed to cancel order');
+    }
+  }
+  async getOrderDetailsByAdmin(id: string) {
+    const order = await this.prisma.orders.findUnique({
+      where: {
+        id: id,
+      },
+      include: {
+        OrderItems: {
+          include: {
+            book: true,
+          },
+        },
+        user: true,
+      },
+    });
+    if (!order) {
+      throw new BadRequestException('Order not found');
+    }
+    return order;
+  }
+  async getOrderHistory(session: TUserSession, dto: OrderPageOptionsDto) {
+    const orders = await this.prisma.orders.findMany({
+      where: { user_id: session.id, ...(dto.status && { status: dto.status }) },
+      include: {
+        OrderItems: {
+          include: {
+            book: true,
+          },
+        },
+      },
+      take: dto.take,
+      skip: dto.skip,
+      orderBy: { [dto.sortBy]: dto.order },
+    });
+    const itemCount = await this.prisma.orders.count({
+      where: { user_id: session.id, ...(dto.status && { status: dto.status }) },
+    });
+    return { orders, itemCount };
+  }
+}
