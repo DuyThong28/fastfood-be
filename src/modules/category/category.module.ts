52a7d2adb4e2dd5d1626adfe1d78070c9b7d8431
minhtai2911 <149917860+minhtai2911@users.noreply.github.com>
2024-12-18 23:40:21 +0700

    fix: module chatbot

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
-import { ChatbotService } from '../chatbot/chatbot.service';
+import { ChatbotModule } from '../chatbot/chatbot.module';
 
 @Module({
-  imports: [PrismaModule, ChatbotService],
+  imports: [PrismaModule, ChatbotModule],
   controllers: [CategoryController],
   providers: [CategoryService],
   exports: [CategoryService],
