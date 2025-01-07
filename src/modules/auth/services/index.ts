fc8f898e87eebfdaaeb74ec756490e871da0d20d
minhtai2911 <149917860+minhtai2911@users.noreply.github.com>
2025-01-07 16:26:50 +0700

    fix bug url

--- src/modules/auth/services/index.ts
+++ src/modules/auth/services/index.ts
@@ -1,6 +1,6 @@
-export { default as SignUpService } from './signup/index';
-export { default as SignInService } from './signin/index';
+export { default as SignUpService } from './signUp/index';
+export { default as SignInService } from './signIn/index';
 export { default as VerificationEmailService } from './verification/index';
-export { default as SignOutService } from './signout/index';
-export { default as RefreshTokenService } from './refreshtoken/index';
-export { default as ForgotPwdService } from './forgotpwd/index';
+export { default as SignOutService } from './signOut/index';
+export { default as RefreshTokenService } from './refreshToken/index';
+export { default as ForgotPwdService } from './forgotPwd/index';
